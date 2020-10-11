5c5b81dcf7fd8ed60ee9365633cf06d364fbc638
Sejal Khanna <67584092+sejalllkhanna@users.noreply.github.com>
2020-10-11 16:56:39 +0530

    Add files via upload

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by sejal khanna on 22/09/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //sejal167kh@gmail.com
+    let body: String
+}
